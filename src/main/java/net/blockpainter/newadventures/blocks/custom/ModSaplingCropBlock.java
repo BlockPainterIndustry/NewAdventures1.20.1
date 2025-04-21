c990ae79a73aab1af928ab63456fdfd75f7ffc05
MagicElf_123 <MagicElf@outlook.de>
2025-04-21 21:54:08 +0200

    Updated Yira Crop Texture

--- src/main/java/net/blockpainter/newadventures/blocks/custom/ModSaplingCropBlock.java
+++ src/main/java/net/blockpainter/newadventures/blocks/custom/ModSaplingCropBlock.java
@@ -14,6 +14,7 @@ import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.CropBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
@@ -23,83 +24,11 @@ import java.util.Properties;
 
 public class ModSaplingCropBlock extends CropBlock {
 
-    public static final int FIRST_STAGE_MAX_AGE = 7;
-    public static final int SECOND_STAGE_MAX_AGE = 1;
-
-    private static final VoxelShape[] SHAPE_BY_AGE = new VoxelShape[]{
-            Block.box(0.0D, 0.0D, 0.0D, 16.0D, 2.0D, 16.0D),
-            Block.box(0.0D, 0.0D, 0.0D, 16.0D, 4.0D, 16.0D),
-            Block.box(0.0D, 0.0D, 0.0D, 16.0D, 6.0D, 16.0D),
-            Block.box(0.0D, 0.0D, 0.0D, 16.0D, 8.0D, 16.0D),
-            Block.box(0.0D, 0.0D, 0.0D, 16.0D, 10.0D, 16.0D),
-            Block.box(0.0D, 0.0D, 0.0D, 16.0D, 12.0D, 16.0D),
-            Block.box(0.0D, 0.0D, 0.0D, 16.0D, 14.0D, 16.0D),
-            Block.box(0.0D, 0.0D, 0.0D, 16.0D, 16.0D, 16.0D),
-            Block.box(0.0D, 0.0D, 0.0D, 16.0D, 16.0D, 16.0D)};
-
-    public static final IntegerProperty AGE = IntegerProperty.create("age", 0, 6);
+    public static final int MAX_AGE = 5;
+    public static final IntegerProperty AGE = BlockStateProperties.AGE_5;
 
     public ModSaplingCropBlock(Properties pProperties) {
         super(pProperties);
-    }
-
-    @Override
-    public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
-        return SHAPE_BY_AGE[this.getAge(pState)];
-    }
-
-    public void randomTick(BlockState pState, ServerLevel pLevel, BlockPos pPos, RandomSource pRandom) {
-        if (!pLevel.isAreaLoaded(pPos, 1)) return;
-        if (pLevel.getRawBrightness(pPos, 0) >= 9) {
-            int currentAge = this.getAge(pState);
-
-            if (currentAge < this.getMaxAge()) {
-                float growthSpeed = getGrowthSpeed(this, pLevel, pPos);
-
-                if (net.minecraftforge.common.ForgeHooks.onCropsGrowPre(pLevel, pPos, pState, pRandom.nextInt((int)(25.0F / growthSpeed) + 1) == 0)) {
-                    if(currentAge == FIRST_STAGE_MAX_AGE) {
-                        if(pLevel.getBlockState(pPos.above(1)).is(Blocks.AIR)) {
-                            pLevel.setBlock(pPos.above(1), this.getStateForAge(currentAge + 1), 2);
-                        }
-                    } else {
-                        pLevel.setBlock(pPos, this.getStateForAge(currentAge + 1), 2);
-                    }
-
-                    net.minecraftforge.common.ForgeHooks.onCropsGrowPost(pLevel, pPos, pState);
-                }
-            }
-        }
-    }
-
-    @Override
-    public boolean canSustainPlant(BlockState state, BlockGetter world, BlockPos pos, Direction facing, IPlantable plantable) {
-        return super.mayPlaceOn(state, world, pos);
-    }
-
-    @Override
-    public boolean canSurvive(BlockState pState, LevelReader pLevel, BlockPos pPos) {
-        return super.canSurvive(pState, pLevel, pPos) || (pLevel.getBlockState(pPos.below(1)).is(this) &&
-                pLevel.getBlockState(pPos.below(1)).getValue(AGE) == 7);
-    }
-
-    @Override
-    public void growCrops(Level pLevel, BlockPos pPos, BlockState pState) {
-        int nextAge = this.getAge(pState) + this.getBonemealAgeIncrease(pLevel);
-        int maxAge = this.getMaxAge();
-        if(nextAge > maxAge) {
-            nextAge = maxAge;
-        }
-
-        if(this.getAge(pState) == FIRST_STAGE_MAX_AGE && pLevel.getBlockState(pPos.above(1)).is(Blocks.AIR)) {
-            pLevel.setBlock(pPos.above(1), this.getStateForAge(nextAge), 2);
-        } else {
-            pLevel.setBlock(pPos, this.getStateForAge(nextAge - SECOND_STAGE_MAX_AGE), 2);
-        }
-    }
-
-    @Override
-    public int getMaxAge() {
-        return FIRST_STAGE_MAX_AGE + SECOND_STAGE_MAX_AGE;
     }
 
     @Override
@@ -110,6 +39,11 @@ public class ModSaplingCropBlock extends CropBlock {
     @Override
     public IntegerProperty getAgeProperty() {
         return AGE;
+    }
+
+    @Override
+    public int getMaxAge() {
+        return MAX_AGE;
     }
 
     @Override
